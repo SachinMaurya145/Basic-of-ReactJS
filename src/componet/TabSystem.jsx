30065b0ab3768d2b91fc2e953058326271e2581f
Sachin <apple@apples-MacBook-Pro-8.local>
2024-07-05 05:24:08 +0530

    switc tab using id

--- src/componet/TabSystem.jsx
+++ src/componet/TabSystem.jsx
@@ -3,28 +3,28 @@ import { tabData } from '../Data/TabData';
 
 function TabSystem() {
   const [tabValue, setTabValue] = useState(tabData[0]);
-  const [activeTab, setActiveTab] = useState(0);
+  const [activeTab, setActiveTab] = useState(tabData[0].id);
 
   const allData = tabData;
 
-  const handleTabClick = (index) => {
-    console.log(" ALL DAT A", index, tabValue  ,activeTab );
-    setTabValue(tabData[index]);
-    setActiveTab(index);
+  const handleTabClick = (id) => {
+    const selectedTab = tabData.find(tab => tab.id === id);
+    setTabValue(selectedTab);
+    setActiveTab(id);
   };
 
   return (
     <div>
-      <h1>This is the Tab System Component</h1>
+      <h1>This is the Tab System Component /// using ID </h1>
       <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
-        {allData.map((val, index) => (
-          <ul key={index}>
+        {allData.map((val) => (
+          <ul key={val.id}>
             <div>
               <button
                 style={{
-                  backgroundColor: activeTab === index ? 'yellow' : 'transparent',
+                  backgroundColor: activeTab === val.id ? 'yellow' : 'transparent', color:"red"
                 }}
-                onClick={() => handleTabClick(index)}
+                onClick={() => handleTabClick(val.id)}
               >
                 {val.tab}
               </button>
